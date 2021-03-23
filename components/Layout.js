35695701c71b65f27824fabf66bafc099e27f2bc
Cristian <crgaonas24@gmail.com>
2021-03-23 16:42:07 -0500

    Agregando page Login y nueva cuenta

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Head from 'next/head';
 import Sidebar from '../components/Sidebar';
+import { useRouter } from 'next/router';
+
+
 
 
 const Layout = ({children}) =>{
-    // Agragr normalize y tilwindcss para estilo de la página
+
+    // Hook de routing
+    const router = useRouter();
+
+    // Agregar normalize y tilwindcss para estilo de la página
     return(
         <>
        
@@ -13,7 +20,14 @@ const Layout = ({children}) =>{
          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
          <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"></link>
          </Head>
-         <div className="bg-gray-200 min-h-screen">
+        {router.pathname === '/login' || router.pathname === '/nuevacuenta' ? (
+            <div className= "bg-gray-800 min-h-screen flex flex-col justify-center">
+               <div>
+               {children}
+               </div>
+            </div>
+        ):(
+            <div className="bg-gray-200 min-h-screen">
             <div className="flex min-h-screen">
 
                 <Sidebar/>
@@ -25,6 +39,11 @@ const Layout = ({children}) =>{
              
            
         </div>
+
+        )}
+
+
+         
 
          
         </>
