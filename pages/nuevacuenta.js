a3da86844d33eaf39b34cf92dd3b6035c0b966be
Cristian <crgaonas24@gmail.com>
2021-03-26 22:31:56 -0500

    Finalizando panatallas de login y nuevacuenta

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -1,13 +1,146 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const NuevaCuenta = () =>{
+
+
+    // Validación del formulario
+
+    const formik = useFormik({
+        initialValues:{
+            nombre: '',
+            apellido : '',
+            email: '',
+            password: ''
+
+        } ,
+
+        validationSchema: Yup.object({
+            nombre: Yup.string().required('El nombre es Obligatorio'),
+            apellido: Yup.string().required('El apellido es Obigatorio'),
+            email: Yup.string().email('El correo no es válido').required('El email es Obligatorio'),
+            password: Yup.string().required('El correo es Obligatorio').min( 6, 'El password debe ser almenos de 6 caracteres'),
+
+
+        }),
+
+        onSubmit: valores=> {
+            console.log('enviando..');
+            console.log(valores);
+
+        }
+    });
     return (
-        <>
         <Layout>
-        <h1 className="text-center text-2xl text-white font-light">Nueva Cuenta</h1>
+        <h1 className="text-center text-2xl text-white font-light">Crear Nueva Cuenta</h1>
+        <div className="flex justify-center mt-5">
+            <div className="w-full max-w-sm"> 
+            <form 
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+            onSubmit={formik.handleSubmit}
+            >
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
+                        Nombre
+                    </label>
+                        <input 
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline"
+                        id="nombre"
+                        type="text"
+                        placeholder="Ingresar nombre"
+                        value={formik.values.nombre}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        />
+                        
+                </div>
+                {formik.touched.nombre && formik.errors.nombre ? (
+                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                        <p className="font-bold">Error</p>
+                        <p>{formik.errors.nombre}</p>
+                    </div>
+
+                ):null}
+
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
+                        Apellido
+                    </label>
+                        <input 
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline"
+                        id="apellido"
+                        type="text"
+                        value={formik.values.apellido}
+                        onChange={formik.handleChange}
+                        placeholder="Ingresar apellido"/>
+                </div>
+
+                {formik.touched.apellido && formik.errors.apellido ? (
+                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                        <p className="font-bold">Error</p>
+                        <p>{formik.errors.apellido}</p>
+                    </div>
+
+                ):null}
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                        Correo
+                    </label>
+                        <input 
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline"
+                        id="email"
+                        type="email"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        placeholder="Ingresar correo"/>
+                </div>
+                {formik.touched.email && formik.errors.email ? (
+                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                        <p className="font-bold">Error</p>
+                        <p>{formik.errors.email}</p>
+                    </div>
+
+                ):null}
+
+                <div className="mb-4">
+
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                        Contraseña
+                    </label>
+                        <input 
+                        className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        placeholder="Ingresar contraseña"/>
+
+                    
+                </div> 
+                {formik.touched.password && formik.errors.password ? (
+                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                        <p className="font-bold">Error</p>
+                        <p>{formik.errors.password}</p>
+                    </div>
+
+                ):null} 
+
+
+                <input 
+                        className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900"
+                         type="submit"
+                        value="Crear cuenta"/>  
+
+                
+                
+            </form>
+            
+            </div>
+        </div>
+        
         </Layout>
-        </>
     );
 
 }
