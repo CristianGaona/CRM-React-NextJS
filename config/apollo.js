4584dd8d514f2287f8cd3f1f2f9023238be7cc1c
Cristian <crgaonas24@gmail.com>
2021-03-30 00:55:17 -0500

    Recupereando el token del localstorage

--- config/apollo.js
+++ config/apollo.js
@@ -1,13 +1,32 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from 'apollo-link-context'
 import fetch from 'node-fetch';
+
+// para trabajar con el contex
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/',
+    fetch
+});
+
+// Recuperamos todo lo que hay en el header pero agregamos nuestro propio header adicional
+const authLink = setContext((_, { headers }) =>{
+
+    // Leer el localstorage
+    const token = localStorage.getItem('token')
+    return{
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}`: ''
+        }
+    }
+});
 
 const client = new ApolloClient({
     connectToDevTools: true,
     cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: 'http://localhost:4000/',
-        fetch
-    })
+    link: authLink.concat( httpLink)
 });
+
+
 
 export default client;
