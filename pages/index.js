bb6735e24a115345a92c77c1b09dd73cf59326ff
Cristian <crgaonas24@gmail.com>
2021-04-03 03:47:53 -0500

    Agregando formulario de cliente integrado con GraphQL

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
 import Layout from '../components/Layout';
 import {gql, useQuery} from '@apollo/client';
+import {useRouter} from 'next/router';
+import Link from 'next/link';
 
 const OBTENER_CLIENTES_USUARIO = gql `
     query obtenerClienteVendedor{
@@ -17,20 +18,29 @@ const OBTENER_CLIENTES_USUARIO = gql `
 
 const Index = () => {
 
-  // Consulta de Apollo
+  const router = useRouter();
 
+  // Consulta de Apollo
   const {data, loading, error } = useQuery(OBTENER_CLIENTES_USUARIO);
-  console.log(data)
-  console.log(loading)
-  console.log(error)
-  if(loading) return 'Cragando'
+  //console.log(data)
+  //console.log(loading)
+  //console.log(error)
+  if(loading) return 'Cargando';
+
+  // Si no hay información 
+  if(!data.obtenerClienteVendedor){
+    return router.push('/login')
+}
   
     return(
       <div>
       <Layout>
           <h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
+          <Link href="/nuevocliente">
+            <a className="bg-blue-800 py-2 px-5 mt-5 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Nuevo Cliente</a>
+          </Link>
 
-          <table className="table-auto shadow-md mt-10 w-full w-lg">
+          <table className="table-auto shadow-md mt-5 w-full w-lg">
             <thead className="bg-gray-800">
               <tr className="text-white">
                 <th className="w-1/5 py-2">Nombre</th>
